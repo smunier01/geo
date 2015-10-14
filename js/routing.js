b588f7d08653cc85b94942f355e262859db471db
Simon M <simon.munier@etu.univ-orleans.fr>
2015-10-14 16:12:21 +0200

    implémentation de dijkstra

--- js/routing.js
+++ js/routing.js
@@ -17,9 +17,11 @@ Routing.prototype.init = function(file) {
 	
 	$.each(result, function(i, field){
 	    that.data[field.source] = [field.x1, field.y1];
+	    that.data[field.target] = [field.x2, field.y2];
             that.graph.addNode(field.source);
 	    that.graph.addNode(field.target);
 	    that.graph.addEdge(field.source, field.target, field.length);
+	    that.graph.addEdge(field.target, field.source, field.length);
 	});
 
 	d.resolve();
@@ -29,8 +31,63 @@ Routing.prototype.init = function(file) {
     return d;
 };
 
-Routing.prototype.dijkstra = function(from, to) {
-    return [from, to];
+Routing.prototype.dijkstra = function (start, stop) {
+    var validatedNodes = [];
+    var visitedNodes = [];
+    visitedNodes[start] = {
+	"dst" : 0,
+	"father" : null
+    };
+    do {
+
+	var selectedNode;
+	var dst = Infinity;
+
+	for (node in visitedNodes) {
+	    if (visitedNodes[node].dst < dst) {
+		selectedNode = node;
+		dst = visitedNodes[node].dst;
+	    }
+	}
+
+	var neighbors = this.graph.getNode(selectedNode)._outEdges;
+
+	for (neighbor in neighbors) {
+
+	    if (neighbor in validatedNodes) {
+		continue;
+	    }
+	    
+	    dst = visitedNodes[selectedNode].dst + neighbors[neighbor].weight;
+	    
+	    if (!(neighbor in visitedNodes)) {
+		
+		visitedNodes[neighbor] = {
+		    "dst" : dst,
+		    "father" : selectedNode
+		};
+	    } else if (visitedNodes[neighbor].dst > dst) {
+		visitedNodes[neighbor] = {
+		    "dst" : dst,
+		    "father" : selectedNode
+		};
+	    }
+
+	}
+	validatedNodes[selectedNode] = visitedNodes[selectedNode];
+	delete visitedNodes[selectedNode];
+
+    } while (selectedNode != stop);
+
+    var path = [];
+    selectedNode = stop;
+    
+    do{
+	path.unshift(selectedNode);
+	selectedNode = validatedNodes[selectedNode].father;
+    } while(selectedNode != null);
+    
+    return path;
 };
 
 Routing.prototype.getRouteFromNodes = function(nodes) {
@@ -38,11 +95,17 @@ Routing.prototype.getRouteFromNodes = function(nodes) {
     var features = [];
 
     for (var i = 0; i < (nodes.length - 1); i++) {
+
 	x1 = this.data[nodes[i+0]][0];
 	y1 = this.data[nodes[i+0]][1];
 	x2 = this.data[nodes[i+1]][0];
 	y2 = this.data[nodes[i+1]][1];
 
+	features.push(new ol.Feature({
+	    'geometry': new ol.geom.Point(
+		ol.proj.transform([x1, y1], 'EPSG:4326', 'EPSG:3857'))
+	}));
+	
 	features.push(new ol.Feature({
 	    'geometry': new ol.geom.LineString(
 		[ol.proj.transform([x1, y1], 'EPSG:4326', 'EPSG:3857'),
