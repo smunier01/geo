6a04abbd73cf04466c8fc9782139bf98f977f297
Simon M <simon.munier@etu.univ-orleans.fr>
2015-12-12 20:58:06 +0100

    fix genJson.php bug

--- php/genJson.php
+++ php/genJson.php
@@ -7,8 +7,8 @@ error_reporting(E_ALL);
 
 try {
 
-    echo 'hello';
-    $command = escapeshellcmd('../ressources/pgsql2geojson.py -d "PG:host=localhost dbname=gis3 user=postgres"');
+    chdir('../ressources/');
+    $command = escapeshellcmd('python ./pgsql2geojson.py -d "PG:host=localhost dbname=gis3 user=postgres"');
     $output = shell_exec($command);
     echo $output;
 
